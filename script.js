2134d8a3e6f33da26916bdacd20afa69c60c5b50
desibasa <desibasagrande@gmail.com>
2025-07-24 12:18:02 +0200

    haleluya

--- script.js
+++ script.js
@@ -11,8 +11,6 @@
  loadComponent("navbar", "navbar.html");
  loadComponent("footer", "footer.html");
 
- //Otro cacho
-
  document.addEventListener("DOMContentLoaded", () => {       // Espera a que se cargue todo el DOM
    const habilidades = document.querySelectorAll('.habilidad');  //Selecciona todos los elementos con la clase habilidad
 
@@ -33,15 +31,6 @@
    });
  });
 
-
-// //OJOOOOOOOOOOOOOOOOOOOOOOOO AQUI VAN LOS OTROS CODIGOS, EL PRIMERO ES EL NAVBAR
-
- let hamburguesa = document.getElementById("hamburguesa");
-
-         function mostrar_lista(){
-             hamburguesa.style.display = "block";
-         }
-
 function loadComponent(id, file, callback) {
     fetch(file)
         .then(response => response.text())
@@ -51,41 +40,3 @@ function loadComponent(id, file, callback) {
         })
         .catch(error => console.error(`Error al cargar ${file}:`, error));
 }
-
-// // Carga navbar con callback
-// loadComponent("navbar", "navbar.html", () => {
-    
-
-//     // Función para mostrar/ocultar la lista
-//     function mostrar_lista() {
-//         if (hamburguesa.style.display === "block") {
-//             hamburguesa.style.display = "none";
-//         } else {
-//             hamburguesa.style.display = "block";
-//         }
-//     }
-
-//     // Asignamos el evento al botón de hamburguesa
-//     menuToggle.addEventListener("click", mostrar_lista);
-// });
-// loadComponent("footer", "footer.html");
-
-// document.addEventListener("DOMContentLoaded", () => {       // Espera a que se cargue todo el DOM
-//   const habilidades = document.querySelectorAll('.habilidad');  //Selecciona todos los elementos con la clase habilidad
-
-//   const observer = new IntersectionObserver((entries, observer) => {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) {
-//         entry.target.classList.add('visible');
-//         observer.unobserve(entry.target); // Deja de observar ese elemento
-//       }
-//     });
-//   }, {
-//     threshold: 0.1 // Aparece cuando el 10% del elemento entra en la vista
-//   });
-
-//   // Empieza a observar cada .habilidad
-//   habilidades.forEach(habilidad => {
-//     observer.observe(habilidad);
-//   });
-// });
