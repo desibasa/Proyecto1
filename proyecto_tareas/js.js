7be6a3492d2c48bfcdb828e12ca3324b9507da2b
desibasa <desibasagrande@gmail.com>
2025-07-25 10:36:13 +0200

    25.0

--- proyecto_tareas/js.js
+++ proyecto_tareas/js.js
@@ -5,10 +5,34 @@ window.addEventListener("load", () => {
 
 let posit = document.getElementById("escribir_posit");
 
+let uno = document.getElementById("uno");
+let dos = document.getElementById("dos");
+let tres = document.getElementById("tres");
+let cuatro = document.getElementById("cuatro");
+let cinco = document.getElementById("cinco");
+let seis = document.getElementById("seis");
+let siete = document.getElementById("siete");
+let ocho = document.getElementById("ocho");
+let nueve = document.getElementById("nueve");
+
+let contador = 0;
+
 function abrir_posit() {
     posit.style.display = "flex";
 
 }
+
 function listo(){
         posit.style.display = "none";
-    }
+        uno.style.display = "block";
+        contador++;
+    };
+
+if(contador==1){
+  function listo(){
+    posit.style.display = "none";
+    dos.style.display = "block";
+    contador++;
+  }
+}
+         
